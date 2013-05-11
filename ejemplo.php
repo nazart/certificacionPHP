d15a3d5795e25fbde1a434efe3fdd4552278f60e
nazart jara <nazartj@gmail.com>
2013-05-11 10:33:57 -0500

    --

--- ejemplo.php
+++ ejemplo.php
@@ -1,6 +1,16 @@
 <?php
-$e = "hola";
-$hola = "hola  mundo";
-echo $$e;
+
+function assing ($e){
+$variable1 = '123';
+$variable2 = '12';
+$variable3 = 'asd';
+return $$e;
+}
+
+echo assing('variable1');
+echo '<p>';
+echo assing('variable2');
+echo '<p>';
+echo assing('variable3');
 
 ?>
